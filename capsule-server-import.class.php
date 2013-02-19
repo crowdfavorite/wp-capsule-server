049b1bdd676b4a51b5e040d91a838315466d14de
Evan Anderson <ejdanderson@gmail.com>
2013-02-19 12:29:45 -0700

    Handle importing of terms properly for each scenario, mapped, unmapped and hierarchical for both

--- capsule-server-import.class.php
+++ capsule-server-import.class.php
@@ -2,12 +2,12 @@
 class Capsule_Server_Import_Post {
 
 	// $data includes three keys:
-	function __construct($api_key, $post, $taxonomies) {
+	function __construct($api_key, $post, $tax_data) {
 		$this->cap_server = new Capsule_Server();
 
 		$this->api_key = $api_key;
 		$this->post = $post;
-		$this->taxonomies = $taxonomies;
+		$this->tax_data = $tax_data;
 		$this->local_post_id = 0;
 
 		$this->set_user();
@@ -52,20 +52,51 @@ class Capsule_Server_Import_Post {
 	}
 
 	function import_terms() {
-		$tax_input = empty($this->post['tax_input']) ? array() : $this->post['tax_input'];
+		// All the taxonomies sent from the client
+		$taxonomies = $this->tax_data['taxonomies'];
+		// The term mappings for each taxonomy
+		$tax_input = $this->tax_data['tax_input'];
+		// The taxonomies which had a local mapping
+		$mapped_taxonomies = $this->tax_data['mapped_taxonomies'];
 
-		foreach ($this->taxonomies as $taxonomy) {
-			if (isset($tax_input[$taxonomy])) {
-				foreach ($tax_input as $terms) {
+		if (!empty($taxonomies)) {
+			foreach ($taxonomies as $taxonomy) {
+				error_log($taxonomy);
+				if (isset($tax_input[$taxonomy])) {
+					$terms = (array) $tax_input[$taxonomy];
+
 					// wp_set_post_terms will use the integers as names if not the correct type
-					$terms = (array) $terms;
-					$terms = array_map('intval', $terms);
-					wp_set_post_terms($this->local_post_id, $terms, $taxonomy);
+					if (in_array($taxonomy, $mapped_taxonomies)) {
+						// This handles forcing hierarchial taxonomies to be have terms as integers
+						
+						wp_set_post_terms($this->local_post_id, $terms, $taxonomy);
+					}
+					else {
+						// Check if hierarchical, need to pass IDs
+						if(is_taxonomy_hierarchical($taxonomy)) {
+							$terms_as_id = array();
+							
+							foreach ($terms as $term) {
+								// Get term, create if not exists
+								$term_id = $this->cap_server_create_term($term, $taxonomy);
+								
+								if ($term_id) {
+									$terms_as_id[] = $term_id;
+								}
+							}
+							
+							wp_set_post_terms($this->local_post_id, $terms_as_id, $taxonomy);
+						}
+						else {
+							wp_set_post_terms($this->local_post_id, $terms, $taxonomy);
+						}
+					}
 				}
-			}
-			else {
-				// There were no terms passed, so unset the current terms
-				wp_set_post_terms($this->local_post_id, array(), $taxonomy);
+				else {
+					error_log($taxonomy);
+					// There were no terms passed, so unset the current terms
+					wp_set_post_terms($this->local_post_id, array(), $taxonomy);
+				}
 			}
 		}
 	}
@@ -90,14 +121,33 @@ class Capsule_Server_Import_Post {
 
 	//@TODO remove user from blog, delete their API key
 	function cap_server_remove_user() {}
+
+	// Similar functionality of wp_create_term but wp_create_term is in wp-admin includes which are not loaded
+	// 
+	function cap_server_create_term($tag_name, $taxonomy) {
+		if ($term_info = term_exists($tag_name, $taxonomy)) {
+			if (is_array($term_info)) {
+				return $term_info['term_id'];
+			}
+
+			return false;
+		}
+
+		$term_info = wp_insert_term($tag_name, $taxonomy);
+		if (is_array($term_info)) {
+			return $term_info['term_id'];
+		}
+		return false;
+	}
 }
 
 class Capsule_Server_Export_Terms {
 	// $data includes three keys:
-	function __construct($api_key = null) {
+	function __construct($api_key = null, $taxonomies = array()) {
 		//$this->cap_server = new Capsule_Server();
 		//$this->api_key = $api_key;
 		//$this->set_user();
+		$this->taxonomies = $taxonomies;
 	}
 
 	//@TODO This should be somewhere else so capsule-server can use it
@@ -128,33 +178,29 @@ class Capsule_Server_Export_Terms {
 	 * 	'taxonomy_2' ....
 	 */ 
 	function get_terms() {
-		$args = array(
-			'object_type' => array('post'),
-		);
-		$taxonomies = get_taxonomies($args);
-		// No Need to map post formats
-		unset($taxonomies['post_format']);
-		//error_log(print_r($taxonomies,1));
+		//@TODO validate user
 		$taxonomy_array = array();
-		$terms = get_terms($taxonomies, array('hide_empty' => false));
 
-		foreach ($terms as $term) {
-			$taxonomy_array[$term->taxonomy][$term->slug] = array(
-				'id' => $term->term_id, 
-				'name' => $term->name,
-				'description' => $term->description,
-				'taxonomy' => $term->taxonomy,
-			);
+		$terms = get_terms($this->taxonomies, array('hide_empty' => false));
+		if (is_array($terms)) {
+			foreach ($terms as $term) {
+				$taxonomy_array[$term->taxonomy][$term->slug] = array(
+					'id' => $term->term_id, 
+					'name' => $term->name,
+					'description' => $term->description,
+					'taxonomy' => $term->taxonomy,
+				);
+			}
 		}
 		return $taxonomy_array;
 	}
 }
 
 
-
 function capsule_server_request_handler() {
 	switch ($_POST['capsule_server_action']) {
 		case 'insert_post':
+			// Cannot use nonce here as they're salted with unique keys, going to have to rely on api key.
 			if (isset($_POST['capsule_client_post_data'])) {
 				$data = $_POST['capsule_client_post_data'];
 				if (isset($data['post']) && isset($data['tax']) && isset($data['api_key'])) {
@@ -169,9 +215,9 @@ function capsule_server_request_handler() {
 			}
 			break;
 		case 'get_terms':
-			$cap_server = new Capsule_Server_Export_Terms();
-			//@TODO Validate here, not in function
-			echo serialize($cap_server->get_terms($_POST['capsule_client_post_data']['api_key']));
+			$term_exporter = new Capsule_Server_Export_Terms($_POST['capsule_client_post_data']['api_key'], $_POST['capsule_client_post_data']['taxonomies']);
+			//@TODO Validate user here, not in function
+			echo serialize($term_exporter->get_terms());
 			die();
 			break;		
 		default:
